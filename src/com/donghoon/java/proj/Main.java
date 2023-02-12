f66ea197f8bf77367feb3fde6371497fc5f5751f
dongne-joomin <donghunc88@gmail.com>
2023-02-12 20:49:18 +0900

    게시물 작성 기능 구현중

--- src/com/donghoon/java/proj/Main.java
+++ src/com/donghoon/java/proj/Main.java
@@ -7,13 +7,46 @@ public class Main {
 		System.out.println("==프로그랜 시작==");
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.printf("명령어 :");
-		String command;
-		command = sc.nextLine();
+		int lastArticleId = 0;
 		
-		System.out.printf("입력된 명령어: %s\n", command);
+		while(true) {
+			System.out.printf("명령어 :");
+			String command = sc.nextLine();
+			
+			command = command.trim();
+			
+			if( command.length() == 0) {
+				continue;
+			}
+			
+			if( command.equals("system exit")) {
+				break;
+			}
+			
+			if( command.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%s글이 생성되었습니다.\n", id);
+			}
+			
+			else if ( command.equals("article list")) {
+				System.out.println("게시물이 없습니다.");
+			}
+			else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
+			}
+		}
+		
+		
+		
 		sc.close();
-		
 		System.out.println("==프로그램 끝==");
 	} 
 }
